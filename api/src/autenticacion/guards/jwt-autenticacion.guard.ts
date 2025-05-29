4ff168edbb500354d21be84915bd16e526126608
Jesus Ruiz <2201080232@undc.edu.pe>
2025-05-29 11:26:12 -0500

    Remove backend files from frontend branch - frontend only deployment

--- api/src/autenticacion/guards/jwt-autenticacion.guard.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-// Este guardia se usa en los controladores para proteger rutas privadas.
-@Injectable()
-export class JwtAutenticacionGuard extends AuthGuard('jwt') {}
